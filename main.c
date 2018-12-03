cf5476f47edd7eb03fe6bcdb2df0649e3d52a129
Victor Luiz Bernardes <30540730+jubileu99@users.noreply.github.com>
2018-12-03 15:02:20 -0200

    Update main.c

--- main.c
+++ main.c
@@ -3,10 +3,10 @@
 #include "matriz.h"
 #include <string.h>
 
-void ler_arquivo(Matriz *m1){
+void ler_arquivo(Matriz *m1,char *dir){
 
     FILE *f;
-    f = fopen("arq1.txt","rt");
+    f = fopen(dir,"rt");
     if(f == NULL){
         m1->status = -1;
     }else{
@@ -23,21 +23,20 @@ void ler_arquivo(Matriz *m1){
 }
 
 int main(int argc,char *argv[]){
-    //if(argc == 3 && strcmp(argv[1],"transpo") == 0){
+    if(argc == 3 && strcmp(argv[1],"transpo") == 0){
         Matriz m1;
-        printf("opa\n");
-        ler_arquivo(&m1);
+        ler_arquivo(&m1,argv[2]);
         if(m1.status == -1){
             printf("Erro ao ler arquivo!. Verifique os arquivos de entrada\n");
             return -1;
         }
         Matriz m2 = transpo(&m1);
-        mostra_matriz(m2);
-        printf("opa1\n");
+        escrever_arquivo(&m2,argv[3]);
+        //mostra_matriz(m2);
         desaloca(&m1);
         desaloca(&m2);
-        //escrever_arquivo(m3,argv[3]);
-    //}
+        
+    }
 
     return 0;
 }
